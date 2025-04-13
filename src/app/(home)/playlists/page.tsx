961926f2af398667b0ab9b4f519dbafc87baaa3b
Bait-Maker <jakubdejworek@gmail.com>
2025-04-13 11:38:28 -0500

    start on yt pully

--- src/app/(home)/playlists/page.tsx
+++ src/app/(home)/playlists/page.tsx
@@ -1,12 +1,46 @@
 import { auth } from "@clerk/nextjs/server";
-import { Sidebar } from "~/components/sidebar/sidebar";
 import { redirect } from "next/navigation";
+import { Pully, Presets } from "pully";
 
-export default async function PlaylistsPage() {
-  const session = await auth();
+export default function PlaylistsPage() {
+  const getSession = async () => {
+    const session = await auth();
 
-  if (!session.userId) {
-    return redirect("/sign-in");
+    if (!session.userId) {
+      return redirect("/sign-in");
+    }
+  };
+
+  getSession();
+
+  async function downloadVideoAction(url: string) {
+    const pully = new Pully();
+
+    const video = await pully.query(
+      "https://www.youtube.com/watch?v=L_jWHffIx5E",
+    );
+    console.log(
+      `${video.videoTitle} by ${video.channelName} has ${video.views} views!`,
+    );
+
+    const options = {
+      url: url,
+      preset: Presets.FourK,
+      progress: (data: { percent: string }) => console.log(data.percent + "%"), // Progress reporter callback...
+    };
   }
-  return <main className="bg-background flex"></main>;
+
+  return (
+    <main className="bg-background flex">
+      <div>
+        <form>
+          <label>Download from Url</label>
+          <input
+            type="text"
+            placeholder="https://www.youtube.com/watch?v=L_jWHffIx5E"
+          />
+        </form>
+      </div>
+    </main>
+  );
 }
